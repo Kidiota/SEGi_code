bc3839bd30e5cebb4ec6556e2a61a41501ec9f04
Kidiota <17718520426@163.com>
2024-03-06 17:32:48 +0800

    master

--- tutorial_5.2/q8.cpp
+++ tutorial_5.2/q8.cpp
@@ -1,5 +1,18 @@
 #include<iostream>
 using namespace std;
 int main(){
-    
+    cout<<"Do you want to run the program?(Y or N):"<<endl;
+    char i;
+    cin>>i;
+    while(i == 'y' || i == 'Y'){
+        int a, b;
+        cout<<"Enter the first number:";
+        cin>>a;
+        cout<<"Enter the second number:";
+        cin>>b;
+        cout<<"The results is "<<a * b<<endl;
+        cout<<"Do you want to run the program again?(Y or N):"<<endl;
+        cin>>i;
+    }
+    return 0;
 }
