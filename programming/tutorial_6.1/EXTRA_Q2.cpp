11e2c7e2c7f4a984a4f62cd451fe1bee68143697
Kidiota <17718520426@163.com>
2024-03-20 17:55:55 +0800

    master

--- programming/tutorial_6.1/EXTRA_Q2.cpp
+++ programming/tutorial_6.1/EXTRA_Q2.cpp
@@ -1,3 +1,27 @@
 #include<iostream>
 using namespace std;
 
+string evenOrodd (int a);
+
+int main(){
+    for (int i; i <= 2; i++){
+        int a;
+        cout<<"Enter an integer: ";
+        cin>>a;
+        cout<<"The number is "<<evenOrodd(a)<<endl<<endl;
+    }
+
+    return 0;
+}
+
+string evenOrodd (int a){
+    string r;
+    a = a % 2;
+    if (a == 0){
+        r = "even";
+    }
+    else{
+        r = "odd";
+    }
+    return r;
+}
